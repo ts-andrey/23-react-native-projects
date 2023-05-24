ddff5f8e6f6935414f9ad448ac3c016dac2772f2
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-05-24 17:56:01 +0300

    feat: set basic navigation

--- expense-tracker/App.js
+++ expense-tracker/App.js
@@ -1,11 +1,32 @@
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import ManageExpense from './screens/ManageExpense';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen/>
+      <BottomTabs.Screen/>
+    </BottomTabs.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <>
-      <View>
-        <Text>Hello World</Text>
-      </View>
+      <StatusBar style='auto' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='ExpensesOverview' component={ExpensesOverview}/>
+          <Stack.Screen name='ManageExpense' component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
